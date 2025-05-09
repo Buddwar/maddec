63794ba08c3d5273e6f15c020dd40e40874ff274
Andreas <h23anber@du.se>
2025-05-09 10:58:53 +0200

    asdasdas
    asdasd

--- Trafik/js/index.js
+++ Trafik/js/index.js
@@ -348,3 +348,37 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   console.log('Swiper initialized:', swiper);
 });
+
+
+    /*Detta är den data som behöver sparas 
+    undan för respektive prenumerant*/
+    let data = {
+    'email': 'h23anber@du.se',
+    'passw': 'hunter23',
+    'fname': 'Andreas',
+    'lname': 'Bergström',
+    'phone': '0761234567',
+    'start': '2025-03-24',//dagens datum
+    'subtype': 'weekly',
+    'paymethod': 'Creditcard',
+    'countrycode': '14',
+    'orgnr': '5512345679',//Organisation nummer behövs för att 
+    //sätta rätt pris på respektive användare, det är något som sker i backend
+    };
+
+
+/*Anropa denna funktion och stoppa in 
+data för användaren*/
+async function create_subscriber(data){
+
+
+    let response = await fetch('https://bergstrom.pythonanywhere.com/create_user', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+    });
+    let jsonResult = await response.json();
+    console.log(jsonResult);
+}
