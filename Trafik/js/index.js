f437da98a5bf91b94e3e68e3a921a4d9339d548a
Andreas <h23anber@du.se>
2025-05-14 12:02:40 +0200

    sdf
    sdf

--- Trafik/js/index.js
+++ Trafik/js/index.js
@@ -316,7 +316,7 @@ subscribeForm.addEventListener('submit', async (e) => {
   };
   
   /* Ser till att användaren valt en betalningsmethod */
-  if (!formData.payment-method) {
+  if (!formData['payment-method']) {
     alert('Vänligen välj en betalningsmetod.');
     return;
   }   
@@ -355,7 +355,8 @@ subscribeForm.addEventListener('submit', async (e) => {
   /*Dock så behöver vi genomföra en betalning innan vi sparar undan användaren*/
 
   //exempel på anrop till functionen
-  let result = await create_subscriber(formData);
+  
+  /*let result = await create_subscriber(formData);
   if (result['Success']){
     // Show success message and close modal
     alert('Tack för din prenumeration! Du kommer att få trafikinformation enligt vald frekvens.');
@@ -369,7 +370,7 @@ subscribeForm.addEventListener('submit', async (e) => {
     subscribeForm.reset();
     alert('Det var problem att skapa upp prenumeranten.', result['Message']);
   }
-});
+});*/
 
 // Initialize map on page load
 document.addEventListener('DOMContentLoaded', async () => {
