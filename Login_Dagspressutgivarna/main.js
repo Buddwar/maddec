4659744c35f0664e34ba2073bc6cb2446ecf55e5
Andreas <h23anber@du.se>
2025-05-22 16:35:36 +0200

    asd
    asd

--- Login_Dagspressutgivarna/main.js
+++ Login_Dagspressutgivarna/main.js
@@ -7,7 +7,7 @@
 
 
 
-
+//TA bort felmeddelandet efter 3 sekunder
 function remove_error_message(id, delay = 3000){
     setTimeout(function() {
         let error_message = document.getElementById(id);
@@ -24,28 +24,45 @@ function remove_error_message(id, delay = 3000){
 const register_button = document.getElementById('login_button');
 //Lägger till en eventlistener på knappen
 register_button.addEventListener('click', async function() {
-    let email = document.getElementById('email').value;
-    let passw = document.getElementById('passw').value;
-    result = await login_customer(email, passw);
-    if (result['Success']){
-        remove_loadingscreen();
-        let error_message = document.getElementById('error_message');
-        error_message.style.backgroundColor = '#99ff5a';
-        error_message.innerHTML = result['Message'];
-        error_message.style.visibility = 'visible';
-        console.log(result);
-        window.location.href="https://www.maddec.online/Dagspressutgivarna/";
+    try{
+        let email = document.getElementById('email').value;
+        let passw = document.getElementById('passw').value;
+        result = await login_customer(email, passw);
+        if (result['Success']){
+            remove_loadingscreen();
+            let error_message = document.getElementById('error_message');
+            error_message.style.backgroundColor = '#99ff5a';
+            error_message.innerHTML = result['Message'];
+            error_message.style.visibility = 'visible';
+            console.log(result);
+            window.location.href="https://www.maddec.online/Dagspressutgivarna/";
+        }
+        else{
+            remove_loadingscreen();
+            display_error_message(result);
+        }
     }
-    else{
+    catch (error){
         remove_loadingscreen();
-        let error_message = document.getElementById('error_message');
-        error_message.style.backgroundColor = '#ff5a5a';
-        error_message.innerHTML = result['Message'];
-        error_message.style.visibility = 'visible';
-        console.log(result);
-        remove_error_message('error_message');
+        display_error_message('Problem uppstod...');
     }
 });
+
+function display_error_message(result){
+    let error_message = document.getElementById('error_message');
+    error_message.style.backgroundColor = '#ff5a5a';
+    error_message.innerHTML = result['Message'];
+    error_message.style.visibility = 'visible';
+    console.log(result);
+    remove_error_message('error_message');
+
+    let email = document.getElementById('email');
+    let passw = document.getElementById('passw');
+    email.style.border = '1px solid #ff5a5a';
+    passw.style.border = '1px solid #ff5a5a';
+    
+    
+}
 
 //Function för att genomföra anrop till en route hos databasen, skapande av kund
 async function login_customer(email, passw) {
