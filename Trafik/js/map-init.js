61b8941368f897f7d082ca25df75e3124ee3d708
estahnilsson <h23estni@du.se>
2025-05-24 10:00:00 +0200

    tagit bort de blåa områdena på kartan vid hovring

--- Trafik/js/map-init.js
+++ Trafik/js/map-init.js
@@ -8,8 +8,15 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-export const situationCluster = L.markerClusterGroup(); // Klustergrupp för situationer (alltså klumpar vi ihop situationer)
-export const cameraCluster = L.markerClusterGroup(); // Klustergrupp för kameror (alltså klumpar vi ihop kameror)
+// Klustergrupp för situationer (alltså klumpar vi ihop situationer)
+export const situationCluster = L.markerClusterGroup({
+  showCoverageOnHover: false //Stänger av det blåa området som visas vid hovring över klustergruppen på kartan.
+});
+
+// Klustergrupp för kameror (alltså klumpar vi ihop kameror)
+export const cameraCluster = L.markerClusterGroup({
+  showCoverageOnHover: false //Stänger av det blåa området som visas vid hovring över klustergruppen på kartan.
+});
 export const roadConditionLines = []; // En tom array som innehåller polylines från Leaflet, vi sparar dessa för att sedan kunna ta bort dem vid byte av län.
 
 // Funktion för att tvinga Leaflet att uppdatera kartans storlek vid fönsterändring
