44f5ffc2585a18b1a2c9a477b1330b7bd3b2122c
Andreas <h23anber@du.se>
2025-05-26 17:35:28 +0200

    test
    asd

--- Trafik/js/subscription.js
+++ Trafik/js/subscription.js
@@ -172,7 +172,7 @@ function setColors(result){
 
     //Sätter fontsize på diverse knappar och text
     let px = parseFloat(result['Data']['fontsize']);
-    let rem = px / 10;
+    let rem = px / 12;
     document.body.style.fontSize = rem + 'rem';
     document.getElementById('prenumeration_title').style.fontSize = 1.5 + 'rem';
 }
