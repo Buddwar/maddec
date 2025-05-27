18942eacc4b301e18f3ce42b22a4bfbd294fcc0e
estahnilsson <h23estni@du.se>
2025-05-27 19:37:17 +0200

    asd

--- Trafik/js/map-init.js
+++ Trafik/js/map-init.js
@@ -1,4 +1,4 @@
-import { geocodeCity } from "./utilities";
+import { geocodeCity } from "./utilities.js";
 
 // Initiera Leaflet-kartan, detta är taget från Leaflet direkt.
 // Vi exporterar kartan, sätter koordinaterna för vart kartan ska visas först, tillsammans med zoom-nivån.
